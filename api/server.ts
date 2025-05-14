1187ec859acbbedd8c176c721511bc1fed4bc9a3
luciano <luc02@live.com>
2025-05-14 11:42:11 -0300

    Ajuste de server

--- api/server.ts
+++ api/server.ts
@@ -1,3 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
-export const server = axios.create({baseURL: 'http://127.0.0.1:8000'})
+export const server = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
